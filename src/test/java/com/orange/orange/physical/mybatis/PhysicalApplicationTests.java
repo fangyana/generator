2192817497618db48a92fc6cc845eb7d8fdee57c
fangyana <836456650@qq.com>
2020-01-02 13:08:17 +0800

    first commit

---
+++ src/test/java/com/orange/orange/physical/mybatis/PhysicalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orange.orange.physical.mybatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhysicalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
